88c4760deb1b3dd8bac2b4b112f9d58db5e21ff1
Marlena Duleba <marlena_duleba@o2.pl>
2024-05-01 13:52:08 +0200

    Add test data

---
+++ src/data/testData.js
@@ -0,0 +1,64 @@
+const people = [
+  {
+    first_name: "Blinni",
+    last_name: "Bedo",
+    age: 74,
+    email: "bbedo0@sitemeter.com",
+  },
+  {
+    first_name: "Ronny",
+    last_name: "Bail",
+    age: 74,
+    email: "rbail1@chronoengine.com",
+  },
+  {
+    first_name: "Craggy",
+    last_name: "Pullinger",
+    age: 84,
+    email: "cpullinger2@t-online.de",
+  },
+  {
+    first_name: "Reagan",
+    last_name: "Pechold",
+    age: 63,
+    email: "rpechold3@imgur.com",
+  },
+  {
+    first_name: "Ronnie",
+    last_name: "Chetwin",
+    age: 70,
+    email: "rchetwin4@shop-pro.jp",
+  },
+  {
+    first_name: "Eugenio",
+    last_name: "Sturgeon",
+    age: 63,
+    email: "esturgeon5@dot.gov",
+  },
+  {
+    first_name: "Lindsay",
+    last_name: "Sturney",
+    age: 85,
+    email: "lsturney6@google.co.jp",
+  },
+  {
+    first_name: "Gabriella",
+    last_name: "Sargint",
+    age: 70,
+    email: "gsargint7@instagram.com",
+  },
+  {
+    first_name: "Tamra",
+    last_name: "Hobgen",
+    age: 85,
+    email: "thobgen8@dedecms.com",
+  },
+  {
+    first_name: "Bartel",
+    last_name: "Darrel",
+    age: 70,
+    email: "bdarrel9@engadget.com",
+  },
+];
+
+module.exports = { people };
