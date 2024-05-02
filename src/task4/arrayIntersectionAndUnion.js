08c7086110645eb8915978c72fe4dca6c458c188
Marlena Duleba <marlena_duleba@o2.pl>
2024-05-02 11:37:41 +0200

    Drop unnecessary logs

--- src/task4/arrayIntersectionAndUnion.js
+++ src/task4/arrayIntersectionAndUnion.js
@@ -1,15 +1,9 @@
 function getArrayIntersection(arr1, arr2) {
-  return arr1.filter(el => arr2.includes(el));
+  return arr1.filter((el) => arr2.includes(el));
 }
 
 function getArrayUnion(arr1, arr2) {
   return [...new Set([...arr1, ...arr2])];
 }
 
-const array1 = [1, 2, 3, 4, 5];
-const array2 = [3, 4, 5, 6, 7];
-
-console.log(getArrayIntersection(array1, array2));
-console.log(getArrayUnion(array1, array2));
-
-module.exports = {getArrayIntersection, getArrayUnion};
+module.exports = { getArrayIntersection, getArrayUnion };
