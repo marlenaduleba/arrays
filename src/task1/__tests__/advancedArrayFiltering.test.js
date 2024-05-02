3451bc342fee500e10e7720c3b9f4f808c4a7926
Marlena Duleba <marlena_duleba@o2.pl>
2024-05-02 11:26:59 +0200

    Reload test data

--- src/task1/__tests__/advancedArrayFiltering.test.js
+++ src/task1/__tests__/advancedArrayFiltering.test.js
@@ -1,48 +1,28 @@
-const {customFilterUnique, uniqueByAge} = require('../advancedArrayFiltering');
+const {
+  customFilterUnique,
+  uniqueByName,
+} = require("../advancedArrayFiltering");
 const { people } = require("../../data/testData");
 
-describe('customFilterUnique', () => {
+describe("customFilterUnique", () => {
+  it("should return an array of unique elements based on the provided callback", () => {
+    const expectedResult = [
+      { name: "Emily Brown", age: 26 },
+      { name: "Emily Doe", age: 40 },
+      { name: "Jane Brown", age: 30 },
+      { name: "Michael Smith", age: 48 },
+      { name: "Jane Doe", age: 35 },
+      { name: "Michael Johnson", age: 54 },
+      { name: "John Brown", age: 38 },
+      { name: "Emily Smith", age: 55 },
+      { name: "John Smith", age: 53 },
+      { name: "Emily Johnson", age: 41 },
+    ];
 
-  it('should return an array of unique elements based on the provided callback', () => {
-    const expectedResult =[
-        {
-          first_name: 'Blinni',
-          last_name: 'Bedo',
-          age: 74,
-          email: 'bbedo0@sitemeter.com'
-        },
-        {
-          first_name: 'Craggy',
-          last_name: 'Pullinger',
-          age: 84,
-          email: 'cpullinger2@t-online.de'
-        },
-        {
-          first_name: 'Reagan',
-          last_name: 'Pechold',
-          age: 63,
-          email: 'rpechold3@imgur.com'
-        },
-        {
-          first_name: 'Ronnie',
-          last_name: 'Chetwin',
-          age: 70,
-          email: 'rchetwin4@shop-pro.jp'
-        },
-        {
-          first_name: 'Lindsay',
-          last_name: 'Sturney',
-          age: 85,
-          email: 'lsturney6@google.co.jp'
-        }
-      ];
-
-    expect(customFilterUnique(people, uniqueByAge)).toEqual(expectedResult);
+    expect(customFilterUnique(people, uniqueByName)).toEqual(expectedResult);
   });
 
-  it('should return an empty array if the input array is empty', () => {
-    expect(customFilterUnique([], uniqueByAge)).toEqual([]);
+  it("should return an empty array if the input array is empty", () => {
+    expect(customFilterUnique([], uniqueByName)).toEqual([]);
   });
-
 });
-
